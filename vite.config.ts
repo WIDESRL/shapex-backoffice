f4c48b4071ba1d916387c3e4f6b8efc3c016f3fa
Floralb Patria <fpatria.ext@oraya.io>
2025-06-10 15:51:16 +0200

    prepare stagig environemnt build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: 'globalThis',
+    'process.env': process.env,
+  },
 })
